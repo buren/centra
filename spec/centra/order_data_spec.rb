5b856448578dd1ab1dc9c5f4b124d42f85d7311e
Jacob Burenstam <burenstam@gmail.com>
2018-05-29 09:21:22 +0200

    Fix order data spec

--- spec/centra/order_data_spec.rb
+++ spec/centra/order_data_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Centra::OrderData do
   describe '#header' do
     it 'can return first column' do
-      csv_string = File.read('spec/data/1-orders.csv')
+      csv_string = File.read('spec/data/10_orders.csv')
       data = Centra::OrderData.new(csv_string)
 
       expect(data.header.first).to eq('Order')
@@ -12,13 +12,30 @@ RSpec.describe Centra::OrderData do
 
   describe '#rows' do
     it 'can return the first row' do
-      csv_string = File.read('spec/data/1-orders.csv')
-      data = Centra::OrderData.new(csv_string)
+      csv_string = File.read('spec/data/10_orders.csv')
+      data = Centra::OrderData.new(csv_string, anonymize: false)
 
       order = data.rows.first
 
-      expect(order.order_date.to_date).to eq(Date.new(2018, 2, 9))
-      expect(order.payment_method_code).to eq('klarna')
+      expect(order.order_date.to_date).to eq(Date.new(2013, 3, 25))
+      expect(order.paytype).to eq('None')
+      expect(order.email).to eq('1908483cb653@example.com')
+    end
+
+    it 'can skip anonymization' do
+      csv_string = File.read('spec/data/10_orders.csv')
+      data = Centra::OrderData.new(csv_string, anonymize: false)
+
+      order = data.rows.first
+      expect(order.email).to eq('1908483cb653@example.com')
+    end
+
+    it 'can skip anonymization' do
+      csv_string = File.read('spec/data/10_orders.csv')
+      data = Centra::OrderData.new(csv_string, anonymize: true)
+
+      order = data.rows.first
+      expect(order.email).not_to eq('1908483cb653@example.com')
     end
   end
 end
