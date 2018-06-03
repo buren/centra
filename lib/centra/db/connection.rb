e80c17bea1f34057882cbcd73c484e5a338cbf11
Jacob Burenstam <burenstam@gmail.com>
2018-06-03 15:19:34 +0200

    Refactor DB::Connection class

--- lib/centra/db/connection.rb
+++ lib/centra/db/connection.rb
@@ -16,21 +16,38 @@ module Centra
           raise(ArgumentError, "unable to require 'pg' gem")
         end
 
-        @connection = PG.connect(
+        @options =  {
           dbname: dbname,
           host: host,
           port: Integer(port),
           user: user,
           password: password,
           connect_timeout: connect_timeout
-        )
+        }
+        @connection = nil
       end
 
+      def connection
+        @connection ||= begin
+          log.debug("Connecting to PG server with options: #{@options}")
+          PG.connect(@options)
+        end
+      end
+
+      # @param [String] sql the SQL-string to execute
       # @return [PG::Result] the result
-      # @see https://deveiate.org/code/pg/PG/Result.html
+      # @see https://deveiate.org/code/pg/PG/Connection.html#method-i-exec-doc
       def execute(sql)
         log.debug("Executing SQL: #{sql}")
-        @connection.exec(sql)
+        connection.exec(sql)
+      end
+
+      # @param [String] sql the copy SQL-string to execute
+      # @return [PG::Result] the result
+      # @see https://deveiate.org/code/pg/PG/Connection.html#method-i-copy_data-doc
+      def copy_data(sql)
+        log.debug("Executing COPY SQL: #{sql}")
+        connection.copy_data(sql) { yield(connection) }
       end
     end
   end
