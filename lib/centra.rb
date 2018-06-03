97909017c5808c9446d7f8eb61a6cad25eddcbbb
Jacob Burenstam <burenstam@gmail.com>
2018-06-03 13:35:08 +0200

    Add Centra::Configuration

--- lib/centra.rb
+++ lib/centra.rb
@@ -1,4 +1,6 @@
 require "centra/version"
+require "centra/null_logger"
+require "centra/loggable"
 
 require "centra/csv_header_converter"
 require "centra/date_range"
@@ -10,4 +12,20 @@ require "centra/order_stats"
 require "centra/rule"
 
 module Centra
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield(configuration) if block_given?
+    configuration
+  end
+
+  class Configuration
+    attr_accessor :logger
+
+    def initialize
+      @logger = NullLogger.new
+    end
+  end
 end
