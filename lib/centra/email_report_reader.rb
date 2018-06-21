788cf7d8c1574f11435de1e5fd4c1d37fc1ebe90
Jacob Burenstam <burenstam@gmail.com>
2018-06-21 17:52:37 +0200

    Add support for reading Centra report emails that lack hostnames

--- lib/centra/email_report_reader.rb
+++ lib/centra/email_report_reader.rb
@@ -2,12 +2,34 @@ require "uri"
 
 module Centra
   class EmailReportReader
-    def initialize(email_body)
-      @urls = URI.extract(email_body, %w(http https))
+    attr_reader :email_body
+
+    def initialize(email_body, hostname: nil)
+      @hostname = hostname
+      @email_body = email_body
+      @urls = nil
+    end
+
+    def absolute_urls
+      @urls ||= URI.extract(email_body, %w(http https))
     end
 
     def report_uri
-      URI.parse(@urls.first)
+      # The first URL in the email is the report file
+      first_url = absolute_urls.first
+      return URI.parse(first_url) if first_url
+
+      # Unfortunately Centra has a bug where only the path to the
+      # report file is present and not a full absolute URL
+      # therefore we need to parse it out ourselves
+      report_path_matcher = /\/[\S]+\/export\?file\=[\S]+.(csv|xls)/
+      url_path = email_body.match(report_path_matcher).to_s
+
+      URI(url_path).tap do |uri|
+        next unless @hostname
+        uri.scheme = 'https'
+        uri.hostname = @hostname
+      end
     end
   end
 end
--- spec/centra/email_report_reader_spec.rb
+++ spec/centra/email_report_reader_spec.rb
@@ -4,16 +4,72 @@ require "centra/email_report_reader"
 
 RSpec.describe Centra::EmailReportReader do
   describe "#report_uri" do
-    it "returns the report URL" do
-      email_body = <<~EMAIL
-      A report was generated. You can view it here
-      https://example.com/ams/export?file=91_1c7ea0.csv
-      EMAIL
+    # file_formats
+    %w[csv xls].each do |file_format|
+      %w[ams ams72 folder].each do |prefix|
+        context "when report URL is absolute" do
+          it "returns the report URL" do
+            email_body = <<~EMAIL
+            A report was generated. You can view it here
+            https://example.com/#{prefix}/export?file=91_1c7ea0.#{file_format}
+            EMAIL
 
-      report_reader = described_class.new(email_body)
+            report_reader = described_class.new(email_body)
 
-      expected = "https://example.com/ams/export?file=91_1c7ea0.csv"
-      expect(report_reader.report_uri.to_s).to eq(expected)
+            expected = "https://example.com/#{prefix}/export?file=91_1c7ea0.#{file_format}"
+            expect(report_reader.report_uri.to_s).to eq(expected)
+          end
+
+          it "returns the report URL and ignores the given hostname" do
+            email_body = <<~EMAIL
+            A report was generated. You can view it here
+            https://example.com/#{prefix}/export?file=91_1c7ea0.#{file_format}
+            EMAIL
+
+            report_reader = described_class.new(email_body, hostname: 'store.example.com')
+
+            expected = "https://example.com/#{prefix}/export?file=91_1c7ea0.#{file_format}"
+            expect(report_reader.report_uri.to_s).to eq(expected)
+          end
+        end
+
+        context "when report URL is missing the hostname" do
+          it "returns the report URL without hostname if no default is given" do
+            email_body = <<~EMAIL
+            A report was generated. You can view it here
+            /#{prefix}/export?file=91_1c7ea0.#{file_format}
+            EMAIL
+
+            report_reader = described_class.new(email_body)
+
+            expected = "/#{prefix}/export?file=91_1c7ea0.#{file_format}"
+            expect(report_reader.report_uri.to_s).to eq(expected)
+          end
+
+          it "returns the report URL with given default hostname" do
+            email_body = <<~EMAIL
+            A report was generated. You can view it here
+            /#{prefix}/export?file=91_1c7ea0.#{file_format}
+            EMAIL
+
+            report_reader = described_class.new(email_body, hostname: 'store.example.com')
+
+            expected = "https://store.example.com/#{prefix}/export?file=91_1c7ea0.#{file_format}"
+            expect(report_reader.report_uri.to_s).to eq(expected)
+          end
+        end
+
+        it "returns an instance of URI" do
+          email_body = <<~EMAIL
+          A report was generated. You can view it here
+          /#{prefix}/export?file=91_1c7ea0.#{file_format}
+          EMAIL
+
+          report_reader = described_class.new(email_body)
+
+          expect(report_reader.report_uri).to be_a(URI)
+        end
+      end
     end
   end
 end
