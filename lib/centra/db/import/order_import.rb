242011c2530a3dad0c495b7af1a749b63c5a81c7
Jacob Burenstam <burenstam@gmail.com>
2018-06-26 12:23:00 +0200

    Refactor DB::OrderImport to use CSVImport class

--- lib/centra/db/import/order_import.rb
+++ lib/centra/db/import/order_import.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
 require 'centra/db/connection'
+require 'centra/db/import/csv_import'
 
 module Centra
   module DB
     class OrderImport
-      def self.call(orders, connection = Connection.new, table_name: Centra.config.database.table_names[:orders])
-        sql = <<~SQL
-          COPY #{table_name}(#{orders.columns.join(',')})
-          FROM STDIN CSV DELIMITER ','
-        SQL
-
-        connection.copy_data(sql) do |conn|
-          orders.each { |order| conn.put_copy_data(order.to_csv) }
-        end
+      def self.call(orders, connection: Connection.new, table_name: Centra.config.database.table_names[:orders])
+        CSVImport.call(products, table_name, connection)
       end
     end
   end
