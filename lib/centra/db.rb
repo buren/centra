067c19e58869657ccd45c43ce9941757506e212d
Jacob Burenstam <burenstam@gmail.com>
2018-06-26 12:46:58 +0200

    Refactor DB requires

---
+++ lib/centra/db.rb
@@ -0,0 +1,12 @@
+require "centra/db/database_tables"
+require "centra/db/connection"
+require "centra/db/migrations"
+
+require "centra/db/import/csv_import"
+require "centra/db/import/order_import"
+require "centra/db/import/product_import"
+
+module Centra
+  module DB
+  end
+end
